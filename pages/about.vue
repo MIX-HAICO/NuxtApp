cd3b3739a0e6d0084ca6809fc3b429fcb1af1e06
haln14 <haln14>
2023-05-28 08:40:53 +0700

    feat: Strcuture project nuxtjs

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <p>This page will be displayed at the /about route.</p>
+    <div>{{ data }}</div>
+  </section>
+</template>
+<script setup lang="ts">
+// Type of `data` is inferred as
+// {
+//   createdAt: {
+//     year: number
+//     month: number
+//     day: number
+//   }
+// }
+const { data } = await useFetch('/api/bar')
+</script>
